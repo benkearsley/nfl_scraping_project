c785c023b33382352221ff330565bcb37f64d926
Ben Kearsley <ben.kearsley@outlook.com>
2023-12-02 16:46:45 -0700

    initialzing sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath('..'))
+
 project = 'NFL Play By Play Analysis'
 copyright = '2023, Benjamin Kearsley & Zayne Maughan'
 author = 'Benjamin Kearsley & Zayne Maughan'
@@ -14,13 +20,20 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages',
+    'myst_parser'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 root_doc = 'index.rst'
 
+source_suffix = ['.rst', '.md']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
