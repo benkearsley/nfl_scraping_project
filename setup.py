d9f343f6eebd4b9aa1001289984a8162d1fbf16b
Ben Kearsley <ben.kearsley@outlook.com>
2023-12-02 15:42:39 -0700

    initialize setup and pyproject.toml

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup, find_packages
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
+with open('requirements.txt', 'r') as f:
+    requirements = f.dread().splitlines()
+
 setup(
     name='nfl_scraping',
     version='0.0.1',
@@ -12,15 +15,7 @@ setup(
     author_email='ben.kearsley@outlook.com',
     url='https://github.com/benkearsley/nfl_scraping_project.git',
     packages=find_packages(),
-    install_requires=[
-        'pandas',
-        'requests',
-        'numpy',
-        'bs4',
-        're',
-        'time'
-        # Do I need to include thins like sphinx?  Or setuptools?  Also is bs4 included in the right way?
-    ],
+    install_requires=requirements,
     long_description=long_description,
     long_description_content_type='text/markdown',
     exclude=['Game Data Collection.ipynb']
